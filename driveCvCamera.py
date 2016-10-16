2f91e50ea844c27fef9f887087e8308d3cf6c864
smandyscom <smandyscom@gmail.com>
2016-10-17 00:02:52 +0800

    .

--- driveCvCamera.py
+++ driveCvCamera.py
@@ -12,20 +12,22 @@ import time
 
 
 class cvCamera(driveCameraBase.cameraBase):
-    def __init__(self, output=None):
+    def __init__(self, output=None, gray_handler=lambda gray_image: gray_image):
         driveCameraBase.cameraBase.__init__(self, output)
         self.capture = cv2.VideoCapture(0)
         self.capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 320)
         self.capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
         self.capture.set(cv2.cv.CV_CAP_PROP_SATURATION, 0.2)
         self.capture.set(cv2.cv.CV_CAP_PROP_FPS, 10)
-
+        # event handler
+        self.__gray_handler = gray_handler
 
     def __serve_method__(self):
         # self.output.seek(0)
         rc, img = self.capture.read()
         if rc:
             imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            self.__gray_handler(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
             jpg = Image.fromarray(imgRGB)
             jpg.save(self.output, 'jpeg')
 
